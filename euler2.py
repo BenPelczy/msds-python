088d145e05f6d4696f48c1f88fba7205b52444b4
BenPelczy <mdg7wj@virginia.edu>
2024-06-14 14:44:51 -0400

    Added euler solutions

---
+++ euler2.py
@@ -0,0 +1,10 @@
+sum = 0
+num1 = 1
+num2 = 2
+while num2 <= 4000000:
+  if num2 % 2 == 0:
+    sum += num2
+  temp = num2
+  num2 += num1
+  num1 = temp
+print(sum)
---
+++ euler3.py
@@ -0,0 +1,10 @@
+""" figure it out
+n = 600851475143
+i = 2
+while i * i <= n:
+    if n % i:
+        i += 1
+    else:
+        n //= i
+print(n)
+"""
