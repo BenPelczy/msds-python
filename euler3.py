85c5e1fc96fcb63d10b58b98e4dabddb22e9b015
BenPelczy <mdg7wj@virginia.edu>
2024-06-16 16:00:05 -0400

    updated solution

--- euler3.py
+++ euler3.py
@@ -1,10 +1,23 @@
-""" figure it out
+def isPrime(n):
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            return False
+    return True
+
+def lpf(n):
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            if isPrime(i):
+                if isPrime(n/i):
+                    return(n/i)
+                else:
+                    return(lpf(n/i))
+
 n = 600851475143
-i = 2
-while i * i <= n:
-    if n % i:
-        i += 1
-    else:
-        n //= i
-print(n)
-"""
+print(lpf(n))
